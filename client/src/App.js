f42f265be2abe352a51f951a80f48d53b2e11676
RougeLight <dantaeflowers@pursuit.org>
2020-01-11 12:21:47 -0500

    working file upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import Login from './Components/Login'
+//import Login from './Components/Login'
 import Profile from './Components/Profile'
-// import Image from './Components/Image'
-import Feed from './Components/Feed'
+import Image from './Components/Image'
+//import Feed from './Components/Feed'
 import './App.css';
 import { BrowserRouter, Link , Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-       <Login/> 
+       {/* <Login/>  */}
   
 
 <Switch>
   <Route path= "/profile" component={Profile} />
-  <Route path= "/feed" component={Feed} />
+  {/* <Route path= "/feed" component={Feed} /> */}
+  <Route path= "/images" component={Image}/>
 </Switch>
 
 
